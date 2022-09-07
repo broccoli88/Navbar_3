6d10b6d266b0e4b155f63b7fbda530e5b139f99f
broccoli88 <kiai@Deus-Ex-Machina.local>
2022-09-07 20:42:58 +0200

    Vue nav sample

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,10 +1,34 @@
 <template>
     <nav :class="{ active: navIsActive }">
-        <div class="nav">
-            <font-awesome-icon icon="fa-solid fa-house-user" />
-            <font-awesome-icon icon="fa-solid fa-gear" />
-            <font-awesome-icon icon="fa-solid fa-book" />
-            <font-awesome-icon icon="fa-solid fa-pen" />
+        <div class="nav" ref="nav">
+            <div class="brand">
+                <span> {{ navIsActive ? "Vue" : "V" }} </span>
+                <span> {{ navIsActive ? "Sample" : "S" }} </span>
+            </div>
+            <div class="nav-icon">
+                <font-awesome-icon icon="fa-solid fa-house-user" />
+                <span class="nav-text" :class="{ 'show-text': navIsActive }"
+                    >Home</span
+                >
+            </div>
+            <div class="nav-icon">
+                <font-awesome-icon icon="fa-solid fa-gear" />
+                <span class="nav-text" :class="{ 'show-text': navIsActive }"
+                    >Setup</span
+                >
+            </div>
+            <div class="nav-icon">
+                <font-awesome-icon icon="fa-solid fa-book" />
+                <span class="nav-text" :class="{ 'show-text': navIsActive }"
+                    >Contacts</span
+                >
+            </div>
+            <div class="nav-icon">
+                <font-awesome-icon icon="fa-solid fa-pen" />
+                <span class="nav-text" :class="{ 'show-text': navIsActive }"
+                    >Message</span
+                >
+            </div>
         </div>
         <font-awesome-icon
             icon="fa-solid fa-angles-right"
@@ -38,18 +62,33 @@ nav {
     transition: 0.4s all ease;
 }
 
+.brand {
+    font-size: 3rem;
+    color: white;
+    font-weight: 800;
+    line-height: 1.1;
+
+    padding: 1rem;
+    margin: 0 calc(50px - 2rem) 5rem;
+
+    display: flex;
+    flex-direction: column;
+}
+
 .fa-angles-right {
     font-size: 2rem;
     color: white;
 
-    margin-top: auto;
+    margin: auto 1rem 1rem;
     align-self: flex-end;
 
     transition: 0.4s all ease;
+
+    cursor: pointer;
 }
 
 .active {
-    width: 200px;
+    width: 250px;
 }
 
 .rotate {
@@ -60,14 +99,35 @@ nav {
     display: flex;
     flex-direction: column;
 }
-.nav > svg {
+
+.nav-icon {
     width: 100%;
-    /* height: 100px; */
+    height: 100px;
 
-    margin: 2rem 0;
-    font-size: 2.5rem;
-    color: white;
+    margin-bottom: 1rem;
 
+    display: flex;
+    align-items: center;
+
+    cursor: pointer;
+}
+
+.nav-icon:hover {
     background-color: var(--color-hover);
+}
+.nav-icon > svg {
+    font-size: 3rem;
+    color: white;
+    margin: calc(50px - 1.5rem);
+}
+
+.nav-text {
+    display: none;
+}
+
+.show-text {
+    display: inline-block;
+    color: white;
+    font-size: 1.3rem;
 }
 </style>
